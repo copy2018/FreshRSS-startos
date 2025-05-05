d4b9d9271893a8b803f688181c2518dfb627d3ec
copy2018 <144347307+copy2018@users.noreply.github.com>
2025-05-05 16:47:04 +0100

    Update migrations.ts

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,4 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-    .fromMapping({}, "1.23.16");
+    .fromMapping({}, "1.26.2");
